51cb5347cfc29a7b8a4b9bd695fcb770430712a1
Gaurav Vyas <140307480+vyas-gaurav@users.noreply.github.com>
2023-11-17 20:41:16 +0530

    Create DAY-15.cpp

---
+++ DAY-15.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   int x;
+	   cin>>x;
+	   if(10>x)
+	   {
+	       cout<<"Thanks for helping Chef!\n";
+	   }
+	   else
+	   {
+	       cout<<-1<<endl;
+	   }
+	}
+	return 0;
+}
