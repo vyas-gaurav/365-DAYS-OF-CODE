306a96bb2750b0a884ff2d741de19ae255b98acf
Gaurav Vyas <140307480+vyas-gaurav@users.noreply.github.com>
2023-12-31 15:51:41 +0530

    Create DAY-58.cpp

---
+++ DAY-58.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;cin>>t;
+	while(t--){
+	    int n,a;cin>>n>>a;int ans=0;int c=0;
+	    cout<<int(sqrt(n))*a<<endl;
+	    
+	}
+	return 0;
+}
